7f6b0815e8b3d7dedf8d9d4a9a3225658e7b60d0
OlivierCoq <coq.olivier36@gmail.com>
2020-10-14 14:06:09 -0400

    security

--- src/components/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyBLRq5T7eO0cNFLqum8JLhJeAlABlQ_b1k",
-    authDomain: "valsalescalc.firebaseapp.com",
-    databaseURL: "https://valsalescalc.firebaseio.com",
-    projectId: "valsalescalc",
-    storageBucket: "valsalescalc.appspot.com",
-    messagingSenderId: "109106514255",
-    appId: "1:109106514255:web:512f9a2d3aee124dbc9d3a"
-}
